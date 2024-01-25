50cb32d77a364c45ed36b1fdc780e0ecb110a643
Thorvald Kjartansson <thorvald.kjartansson@uniwise.dk>
2024-01-25 13:07:53 +0100

    Add action dummy data

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,24 @@
 <template>
   <div id="main">
     <h1>{{ msg }}</h1>
-  
-    <div class="card">
-      <button type="button" @click="increment">count is {{ actions.count }}</button>
+    <div class="action" v-for="action in actions.defined">
+      <div>
+        <button class="name" type="button" @click="increment">{{ action.name }}</button>
+        <div class="advantage">
+          <button type="button" :class="{ active: advantage}" @click="advantage">ADV</button>
+          <button type="button" :class="{ active: disadvantage}" @click="disadvantage">DISADV</button>
+        </div>
+      </div>
+      <div>
+        <p class="tohit">To hit: {{ (action.bonus<0?"":"+") + action.bonus }}</p>
+        <p class="damage">
+          Damage: 
+          <span v-for="(damage, i) in action.damage">
+            {{ damage.dice }}{{ (damage.bonus<0?"":"+") + damage.bonus }} {{ damage.type }} {{ i<action.damage.length-1 ? "+ " : ""}}
+          </span>
+        </p>
+        <p class="save">Save: {{ action.save ? "DC"+action.save : "none" }}</p>
+      </div>
     </div>
   </div>
 </template>
--- src/obr.js
+++ src/obr.js
@@ -5,6 +5,28 @@ export default {
     return {
       actions: {
         count: 0,
+        profiency: 2,
+        defined: [
+          {
+            name: 'Unarmed Strike',
+            bonus: 4,
+            advantage: false,
+            disadvantage: false,
+            save: 15,
+            damage: [
+              {
+                dice: '1d4',
+                bonus: 2,
+                type: 'bludgeoning'
+              },
+              {
+                dice: '1d4',
+                bonus: 2,
+                type: 'bludgeoning'
+              },
+            ]
+          }
+        ]
       },
       msg: 'Hello OBR!'
     }
@@ -22,11 +44,12 @@ export default {
     if (localStorage.getItem('actions') === null) {
       localStorage.setItem('actions', JSON.stringify(this.actions))
     } else {
-      this.actions = JSON.parse(localStorage.getItem('actions'))
+      // this.actions = JSON.parse(localStorage.getItem('actions'))
     }
     OBR.player.getName().then(name => {
       OBR.notification.show(`Hello ${name}!`)
       this.msg = `Hello ${name}!`
     })
+    OBR.action.setWidth(500)
   }
 }
